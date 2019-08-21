9c586292ccec4f0ffa0b71bafe933fe9131cac7a
Tanaka-Tomoya <tomoya2627@gmail.com>
2019-08-22 03:12:28 +0900

    first commit

---
+++ src/server/index.ts
@@ -0,0 +1,14 @@
+import Express from 'express';
+const app = Express();
+
+app.get('/', (req, res) => {
+  const data = { message: 'pong' };
+  res.send(data);
+});
+
+const port = 8888;
+const host = 'localhost';
+
+app.listen(port, host, () => {
+  console.log(`Running on http://${host}:${port}`);
+});
